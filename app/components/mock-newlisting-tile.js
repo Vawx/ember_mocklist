512c108b2119657c0f69bc82c90cfc09fa78ba98
kyle <altck8@gmail.com>
2016-02-29 17:32:28 -0800

    add saving of new listing for category

--- app/components/mock-newlisting-tile.js
+++ app/components/mock-newlisting-tile.js
@@ -2,11 +2,30 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
     showAddListing: false,
+
+    category: Ember.computed('categoryIndex', function( ) {
+      return this.get('categories')[this.get('categoryIndex')];
+    }),
     actions: {
       showNewListForm( ) {
         this.set("showAddListing", true);
       },
       hideNewListing( ) {
+        this.set("showAddListing", false);
+      },
+      didMakeSelection( value, component ) {
+        this.sendAction('didMakeSelection', value, component );
+      },
+      addListing( ) {
+        var params = {
+          name: this.get("listing"),
+          info: this.get("info"),
+          category: this.get("category"),
+        };
+        this.sendAction('addListing', params);
+        this.set("listing", "");
+        this.set("info", "");
+        this.set("currentCategory", "Ruby");
         this.set("showAddListing", false);
       }
     }
--- app/routes/index.js
+++ app/routes/index.js
@@ -1,6 +1,7 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+
   model( ) {
     return Ember.RSVP.hash({
       categories: this.store.findAll("mock-category"),
@@ -8,6 +9,34 @@ export default Ember.Route.extend({
     });
   },
   actions: {
-    
+    addListing( params ) {
+
+      if(params.name.length > 0 && params.info.length > 0) {
+        var newListingParams = {
+          name: params.name,
+          info: params.info,
+        };
+        var newListing = this.store.createRecord('mock-listing', newListingParams);
+        this.store.find('mock-category', params.category).then( function( category ) {
+          category.get("listings").addObject( newListing );
+          category.save( ).then( function( ) {
+            return newListing.save( );
+          });
+        });
+        this.transitionTo('index');
+      }
+    }
   }
 });
+
+/**
+var newListingParams = {
+  name: params.name,
+  info: params.info,
+};
+ var newListing = this.store.createRecord('mock-listing', newListingParams );
+ category.get('listings').addObject( newListing );
+ category.save( ).then( function( ) {
+   return newListing.save( );
+ });
+ */
